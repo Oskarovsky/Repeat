990c8d49ead186e456a279eaf105c10db7913f60
oskarro <oskar.slyk@gmail.com>
2019-02-11 22:00:24 +0100

    Adding menu bar

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -29,3 +31,9 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts, visits=visits)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
