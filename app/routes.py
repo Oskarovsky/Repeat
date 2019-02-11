8a9064c0fd6b68815f9aad67de2651641071f199
oskarro <oskar.slyk@gmail.com>
2019-02-11 21:49:16 +0100

    Adding first elements of main view page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Oskar'}
+    posts = [
+        {
+            'author': {'username': 'Gosia'},
+            'body': 'I have made new chocolate cake'
+        },
+        {
+            'author': {'username': 'Misio'},
+            'body': 'This is my dinner, which i did today'
+        }
+    ]
+
+    visits = [
+        {
+            'author': {'username': 'Oski'},
+            'body': 'It was really great pleasure!'
+        },
+        {
+            'author': {'username': 'Dosia'},
+            'body': 'BMG is one of the best restaurant in Warsaw'
+        },
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts, visits=visits)
