2dd05cb399e1744526fdf517e26338bb1b21aefd
oskarro <oskar.slyk@gmail.com>
2019-02-12 13:59:36 +0100

    Creating Database

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # location of application's database is from SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # this is set to False to disable a feature of Flask-SQLAlchemy that we don't need
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
