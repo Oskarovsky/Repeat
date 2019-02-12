90cb4167978db3d3d347d30149136c6fe94ceb46
oskarro <oskar.slyk@gmail.com>
2019-02-12 15:43:16 +0100

    Shell context

--- repeat.py
+++ repeat.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post, Visit
+
+# This function creates a shell context that adds the database instance and models to the shell session
+# After adding the shell context processor function we can work with database entities without having to import them
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Visit': Visit}
