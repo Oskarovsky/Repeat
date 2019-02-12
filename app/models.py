cb25eb9b99c466e5cfa46f918250b02d391a618c
oskarro <oskar.slyk@gmail.com>
2019-02-12 14:52:51 +0100

    Update SQL tables

--- app/models.py
+++ app/models.py
@@ -1,11 +1,39 @@
+from datetime import datetime
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    visits = db.relationship('Visit', backref='author', lazy='dynamic')
 
     # this method tells how to print objects of this class, which is going to be useful for debugging
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    food_type = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
+
+
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    food_type = db.Column(db.String(140))
+    place = db.Column(db.String(140))
+    rate = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Visit {}>'.format(self.body)
