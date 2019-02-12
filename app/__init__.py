1b02ff8795f0d5fb3bfc5a7b2ff37f4c9f7cbbb4
oskarro <oskar.slyk@gmail.com>
2019-02-12 14:15:15 +0100

    Creating The Migration Repository

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Config
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)    # object that represents the database
+
+migrate = Migrate(app, db)  # object that represents migration engine
+
+from app import routes, models
