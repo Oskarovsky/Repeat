63c93f7da7c24fc22db49b74d764d53d51398852
oskarro <oskar.slyk@gmail.com>
2019-02-13 13:11:23 +0100

    Logging User In

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import Config
 
@@ -11,5 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)    # object that represents the database
 
 migrate = Migrate(app, db)  # object that represents migration engine
+
+login = LoginManager(app)   # object for managing the user logged-in state
 
 from app import routes, models
