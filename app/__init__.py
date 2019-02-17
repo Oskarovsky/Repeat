90e14f9c08edece231b4092bdb18d807258e3b69
oskarro <oskar.slyk@gmail.com>
2019-02-17 15:24:20 +0100

    Added email handler for errors

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 from config import Config
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 
@@ -17,3 +19,21 @@ login = LoginManager(app)   # object for managing the user logged-in state
 login.login_view = 'login'  # function (or endpoint) name for the login view. this is the name to use in a url_for()
 
 from app import routes, models, errors
+
+# function for sending out emails on errors (it is only running without DEBUG MODE)
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIN_USE_TLS']:
+            secure=()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='RepEAT Failure!',
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
