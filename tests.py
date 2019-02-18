4db86e5aa29b241b8dab2ae0f978b077d7c1494c
oskarro <oskar.slyk@gmail.com>
2019-02-18 15:30:19 +0100

    All tests works !

--- tests.py
+++ tests.py
@@ -25,6 +25,7 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u.check_password('1111'))
         self.assertTrue(u.check_password('1234'))
 
+    # test that exercise the the follow option
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
@@ -85,6 +86,45 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(f2, [p2, p3])
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
+
+    def test_follow_visits(self):
+        # create four user
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        # create four visits
+        now = datetime.utcnow()
+        v1 = Visit(body="visit from john", author=u1,
+                   timestamp=now+timedelta(seconds=1))
+        v2 = Visit(body="visit from susan", author=u2,
+                   timestamp=now+timedelta(seconds=4))
+        v3 = Visit(body="visit from mary", author=u3,
+                   timestamp=now+timedelta(seconds=3))
+        v4 = Visit(body="visit from david", author=u4,
+                   timestamp=now+timedelta(seconds=2))
+        db.session.add_all([v1, v2, v3, v4])
+        db.session.commit()
+
+        # setup the followers
+        u1.follow(u2)  # john follows susan
+        u1.follow(u4)  # john follows david
+        u2.follow(u3)  # susan follows mary
+        u3.follow(u4)  # mary follows david
+        db.session.commit()
+
+        # check the followed visits of each user
+        f1 = u1.followed_visits().all()
+        f2 = u2.followed_visits().all()
+        f3 = u3.followed_visits().all()
+        f4 = u4.followed_visits().all()
+        self.assertEqual(f1, [v2, v4, v1])
+        self.assertEqual(f2, [v2, v3])
+        self.assertEqual(f3, [v3, v4])
+        self.assertEqual(f4, [v4])
+
 
 
 
